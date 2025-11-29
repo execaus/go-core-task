a9d78a95dcf222791a74a2873372ea9ca6eae7a6
execaus <execaus@gmail.com>
2025-11-29 13:33:39 +0500

    T-6 Добавляет реализацию

--- 6/main.go
+++ 6/main.go
@@ -1,18 +1,30 @@
 package main
 
+import (
+	"math/rand"
+)
+
 type UnbufferRandomGenerator struct {
-	notifier       chan int
 	maxRandomValue int
+	notifier       chan int
 }
 
+// NewUnbufferRandomGenerator создаёт новый генератор случайных чисел
+// с неблокирующим (небуферизированным) каналом для отправки значений.
 func NewUnbufferRandomGenerator(maxRandomValue int) *UnbufferRandomGenerator {
-	return &UnbufferRandomGenerator{}
+	return &UnbufferRandomGenerator{
+		maxRandomValue: maxRandomValue,
+		notifier:       make(chan int),
+	}
 }
 
+// Channel возвращает канал, по которому генератор отправляет значения.
 func (g *UnbufferRandomGenerator) Channel() chan int {
-	// TODO
+	return g.notifier
 }
 
+// Generate генерирует случайное число в диапазоне [0, maxRandomValue)
+// и отправляет его в канал. Вызов блокируется, пока получатель не прочитает значение.
 func (g *UnbufferRandomGenerator) Generate() {
-	// TODO
+	g.notifier <- rand.Intn(g.maxRandomValue)
 }
